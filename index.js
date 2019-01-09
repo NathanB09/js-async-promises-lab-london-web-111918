a8508edf1104f64267f4015b79d50aa5ba37489a
NathanB09 <nathanbell09@gmail.com>
2019-01-09 18:12:15 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,54 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
 
+let randQ = questions[Math.floor(Math.random() * questions.length)];
 let question;
+
+function appendQuestion(question) {
+  const qContainer = document.querySelector(".question-container");
+  qContainer.innerHTML = question.questionText;
+}
+
+function askQuestionThen() {
+  return new Promise((resolve) => resolve(question = randQ));
+}
+
+function removeQuestion() {
+  const qContainer = document.querySelector(".question-container");
+  qContainer.innerHTML = '';
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  appendQuestion(questions[0]);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(removeQuestion());
+    }, time)
+  })
+}
+
+function trueAndFalseButtons() {
+  return document.querySelectorAll(".btn.center-align");
+}
+
+function toggleTrueAndFalseButtons() {
+  let buttons = document.querySelectorAll(".btn.center-align");
+  buttons.forEach((btn) => {
+    // if (btn.classList.contains('hide')) {
+    //   btn.classList.remove('hide');
+    // } else {
+    //   btn.classList.add('hide');
+    // }
+    btn.classList.toggle('hide')
+  })
+}
+
+function both() {
+  toggleTrueAndFalseButtons();
+  askQuestionThenRemoveQuestion(5000);
+}
+
+function displayQuestionOnClick() {
+  let askBtn = document.querySelector('a.btn');
+  askBtn.addEventListener('click', both);
+}
